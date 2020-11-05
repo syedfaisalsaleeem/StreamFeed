4b0fbfdbc6cedd1ed91068cbf523b8c5fbb817ff
syedfaisalsaleeem <syedfaisalsaleem.100@gmail.com>
2020-11-05 09:49:04 +0000

    9:48

--- checking.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-
-today = date.today()
-print(today)
-d2 = today.strftime("%Y")
-d1 = today.strftime("-%m-%d")
-lastyear=str(int(d2)-1)+d1
-print(lastyear)
-# print("d1 =", str(int(d2)-1)+d1)
